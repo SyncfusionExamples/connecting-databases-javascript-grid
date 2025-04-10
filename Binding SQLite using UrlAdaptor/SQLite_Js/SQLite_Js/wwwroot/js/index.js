1debbae732e555ed7bd2b5d28be5a6fc350a05ea
Gayathri4135 <gayathri.palanivel@syncfusion.com>
2025-04-10 17:17:57 +0530

    Update the database

---
+++ Binding SQLite using UrlAdaptor/SQLite_Js/SQLite_Js/wwwroot/js/index.js
@@ -0,0 +1,29 @@
+﻿
+ej.grids.Grid.Inject(ej.grids.Toolbar, ej.grids.Edit, ej.grids.Filter, ej.grids.Page);
+
+var data = new ej.data.DataManager({
+    url: 'https://localhost:7061/api/Grid',
+    insertUrl: 'https://localhost:7061/api/Grid/Insert',
+    updateUrl: 'https://localhost:7061/api/Grid/Update',
+    removeUrl: 'https://localhost:7061/api/Grid/Remove',
+   // batchUrl: 'https://localhost:7061/api/Grid/BatchUpdate',
+    adaptor: new ej.data.UrlAdaptor()
+});
+
+
+var grid = new ej.grids.Grid({
+    dataSource: data,
+    toolbar: ['Add', 'Edit', 'Delete', 'Update', 'Cancel', 'Search'],
+    allowPaging: true,
+    allowSorting: true,
+    allowFiltering: true,
+    editSettings: { allowEditing: true, allowAdding: true, allowDeleting: true, mode: 'Normal' },
+    columns: [
+        { field: 'OrderID', headerText: 'Order ID', isIdentity: true, isPrimaryKey: true, textAlign: 'Right', width: 120 },
+        { field: 'CustomerID', headerText: 'Customer ID', width: 150 },
+        { field: 'EmployeeID', headerText: 'Employee ID', width: 160 },
+        { field: 'Freight', headerText: 'Freight', format: 'C2', width: 150, textAlign: 'Right' },
+        { field: 'ShipCity', headerText: 'Ship City', width: 140 }
+    ],
+});
+grid.appendTo('#Grid');
